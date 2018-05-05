9d3fec9f7be8d7c7606591f434114ddba8ca9738
rogersonpi <iain.rogerson77@googlemail.com>
2018-05-05 10:31:20 +0100

    created save and delete methods in artist class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,23 @@
 require_relative( "../models/album.rb" )
-#require_relative( "../models/artist.rb" )
+require_relative( "../models/artist.rb" )
 
 require("pry-byebug")
 
 Album.delete_all()
 
 album1 = Album.new ({
-
   "title" => "Sleeping through the war",
   "genre" => "neo-psychedelia",
   "quantity" => "10",
   "price" => "1100",
   "cost" => "300"})
 album1.save()
+
+artist1 = Artist.new ({
+  "name" => "All them Witches",
+  "album_id" => album1.id
+  })
+artist1.save()
 
 binding.pry
 nil
